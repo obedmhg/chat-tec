a126dae1987fb5450c9dea6e1b67b5a725ff9561
Obed Murillo <obedmhg@gmail.com>
2021-11-25 14:53:54 -0700

    Adding code to my repo

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useRef, useState } from 'react';
+import './App.css';
+
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+    // your config
+});
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
+function App() {
+
+  const [user] = useAuthState(auth);
+
+  return (
+    <div className="App">
+      <header>
+        <h1>⚛️🔥💬</h1>
+        <SignOut />
+      </header>
+
+      <section>
+        {user ? <ChatRoom /> : <SignIn />}
+      </section>
+
+    </div>
+  );
+}
+
+function SignIn() {return <div></div>}
+function SignOut() {return <div></div>}
+function ChatRoom() {return <div></div>}
+function ChatMessage() {return <div></div>}
+
+export default App;
